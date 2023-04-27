c1598ede23ad2b5340908968b9e2e5325de993df
Jakob <114905684+JakobEl80@users.noreply.github.com>
2023-04-27 15:45:08 +0200

    ProductEntity

--- WebApi/Entities/UserProfileEntity.cs
+++ WebApi/Entities/UserProfileEntity.cs
@@ -2,5 +2,13 @@
 {
     public class UserProfileEntity
     {
+        public string UserId { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string StreetAddress { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
     }
 }
