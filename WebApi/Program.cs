afcfc3eb292be4f286ba51cbf073e38efcde6d37
Daniel Novacic <daniel.novacic92@gmail.com>
2023-05-01 11:47:07 +0200

    Uppdaterade Program.CS och la till repos/services/context

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,25 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Contexts;
+using WebApi.Repos;
+using WebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
+
+
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DataDB")));
+
+builder.Services.AddScoped<ProductRepo>();
+builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<CategoryRepo>();
+
+
+
+
+
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
