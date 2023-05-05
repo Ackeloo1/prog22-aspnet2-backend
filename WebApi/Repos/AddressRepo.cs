567ab1aae35c4ba450f6f55ca7d76afb89365706
Daniel Novacic <daniel.novacic92@gmail.com>
2023-05-05 15:15:07 +0200

    Created Address controller, service, and repo

---
+++ WebApi/Repos/AddressRepo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Contexts;
+using WebApi.Models.Entities;
+
+namespace WebApi.Repos;
+
+public class AddressRepo : BaseRepo<AddressEntity, DataContext>
+{
+	public AddressRepo(DataContext context) : base(context)
+	{
+	}
+
+
+	public async Task<AddressEntity> GetByIdAsync(int id)
+	{
+		return await base.GetAsync(x => x.AddressId == id);
+	}
+
+	public async Task<IList<AddressEntity>> SearchAsync(string searchString)
+	{
+		return await _context.Addresses
+			.Where(x =>
+			x.Street.Contains(searchString) ||
+			x.City.Contains(searchString) ||
+			x.Country.Contains(searchString) ||
+			x.PostalCode.Contains(searchString))
+		.ToListAsync();
+	}
+}
